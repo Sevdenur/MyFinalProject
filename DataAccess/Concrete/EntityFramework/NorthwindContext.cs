f99cbca0d75ac7b883149d58021fd5a17f356656
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-02-09 10:53:37 +0300

    9 şubat

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -12,7 +12,6 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\v11.0;Database=Northwind;Trusted_Connection=true");
-            //Data Source = (localdb)\v11.0; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
         }
 
         public DbSet<Product> Products { get; set; }
