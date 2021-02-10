4de2c6a4378acfb328b0e34fc6326abb0296d95e
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-02-10 14:41:53 +0300

    10 Şubat commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,15 +11,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //IoC
+            //CategoryTest();                    
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             Console.WriteLine("---------------All products listed-------------------");
             foreach (var product in productManager.GetAll())
-            {           
+            {
                 Console.WriteLine(product.ProductName);
             }
 
             Console.WriteLine("---------------Products listed by desired unit price-----------------------");
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
@@ -28,6 +44,12 @@ namespace ConsoleUI
             foreach (var product in productManager.GetAllByCategoryId(5))
             {
                 Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("-----------------Products listed with category name--------------------");
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
